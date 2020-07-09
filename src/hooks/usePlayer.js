389124d4d121ba7475c91090cdfebfc9374b2d9c
eXConf <sergey.sosedkin@gmail.com>
2020-07-09 14:28:49 +0300

    Initial commit

---
+++ src/hooks/usePlayer.js
@@ -0,0 +1,30 @@
+import { useState, useCallback } from 'react'
+
+import { randomTetromino, TETROMINOS } from '../tetrominos'
+import { STAGE_WIDTH } from '../gameHelpers'
+
+export const usePlayer = () => {
+    const [player, setPlayer] = useState({
+        pos: {x: 0, y: 0},
+        tetromino: TETROMINOS[0].shape,
+        collided: false
+    })
+    //console.log(player)
+    const updatePlayerPos = ({ x, y, collided }) => {
+        setPlayer(prev => ({
+            ...prev,
+            pos: { x: (prev.pos.x += x/2), y: (prev.pos.y += y/2) },
+            collided
+        }))
+    }
+
+    const resetPlayer = useCallback(() => {
+        setPlayer({
+            pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+            tetromino: randomTetromino().shape,
+            collided: false
+        })
+    }, [])
+
+    return [player, updatePlayerPos, resetPlayer]
+}
