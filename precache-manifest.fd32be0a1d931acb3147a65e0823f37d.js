fb47dfef5cbc237de8885838d555e6ef91d92338
eXConf <sergey.sosedkin@gmail.com>
2020-07-10 20:20:14 +0300

    Updates

---
+++ precache-manifest.fd32be0a1d931acb3147a65e0823f37d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005f3df043e574bf269fbe94aa0588a6",
+    "url": "/react-tetris-tutorial/index.html"
+  },
+  {
+    "revision": "018f3d29b6d2d102d7bc",
+    "url": "/react-tetris-tutorial/static/css/main.5865e1c5.chunk.css"
+  },
+  {
+    "revision": "a4036a24f950e6800b91",
+    "url": "/react-tetris-tutorial/static/js/2.d9129e91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tetris-tutorial/static/js/2.d9129e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "018f3d29b6d2d102d7bc",
+    "url": "/react-tetris-tutorial/static/js/main.a9ef38a9.chunk.js"
+  },
+  {
+    "revision": "5d35b7c91f546ffb268f",
+    "url": "/react-tetris-tutorial/static/js/runtime-main.a042389b.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris-tutorial/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris-tutorial/static/media/bg.2810fe6b.png"
+  }
+]);
